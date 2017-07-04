25c2cd0cfdca83fb36ad73bbb337e5a794916fdd
priya3096 <priyamandal1974@gmail.com>
2017-07-04 11:29:35 +0530

    added server file

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+//your routes here
+var root = process.cwd();
+app.get('/', function (req, res) {
+    res.sendFile('views/index.html',{root});
+});
+app.get('/home', function (req, res) {
+    res.sendFile('views/home.html',{root});
+});
+app.get('/output', function (req, res) {
+    res.sendFile('views/output.html',{root});
+});
+app.use(express.static('public'));
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
